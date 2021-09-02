3009b445b685c06a948bfb437bd56b16286ed2db
Ritik kumar <thakurritik38921@gmail.com>
2021-09-03 01:29:38 +0530

    counter project with the help of html , css , js

--- ritik.js
+++ ritik.js
@@ -1,17 +1,45 @@
-const btn1 = document.querySelector('.btn');
-btn1.style.position = 'relative';
-btn1.style.marginLeft = "48vw";
-btn1.style.padding = '10px 20px 10px 20px';
+const val = document.querySelector('.value');
+const dec = document.querySelector('#decrease');
+const reset = document.querySelector('#reset');
+const inc = document.querySelector('#increase');
+//reset
 
-const span = document.querySelector('.span');
+
+reset.addEventListener('click',function(){
+  val.textContent = 0;
+  if(parseInt(val.textContent) > 0){
+    val.style.color = 'green';
+  }else if(parseInt(val.textContent) < 0){
+    val.style.color = 'red';
+  }else{
+    val.style.color = 'black';
+  }
+});
+
+// decrease 
+dec.addEventListener('click',function(){
+ let store = val.textContent;
+
+ val.textContent = parseInt(store) - 1;
+ if(parseInt(val.textContent) > 0){
+  val.style.color = 'green';
+}else if(parseInt(val.textContent) < 0){
+  val.style.color = 'red';
+}else{
+  val.style.color = 'black';
+}
+});
+//increase
 
 
-btn1.style.color = 'red';
-const b = document.querySelector('body');
-simple = ['red','green','violet','gray','yellow','orange','blue','black','cyan','silver','rust','brown','purple','maroon','gold'];
-btn1.addEventListener('click',function(){
-  let len10 = simple.length;
-  let r = parseInt(len10*Math.random());
-  b.style.backgroundColor = simple[r];
-  span.textContent = simple[r];
+inc.addEventListener('click',function(){
+ let store = val.textContent;
+ val.textContent = parseInt(store) + 1;
+ if(parseInt(val.textContent) > 0){
+  val.style.color = 'green';
+}else if(parseInt(val.textContent) < 0){
+  val.style.color = 'red';
+}else{
+  val.style.color = 'black';
+}
 });
